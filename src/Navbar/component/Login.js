dbe5585b81d2c8fc02b0e735fcdd8a033c1da9bd
yashk1931 <yashk1931@gmail.com>
2026-03-16 22:13:39 +0500

    Update Login.js

--- src/Navbar/component/Login.js
+++ src/Navbar/component/Login.js
@@ -14,7 +14,7 @@ export default function Login({ setCurrentUser }) {
 
     try {
 
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
